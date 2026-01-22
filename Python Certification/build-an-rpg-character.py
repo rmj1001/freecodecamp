d9fae73258f203c2831ceb1c0d585450f350f061
Roy Conn <rmichael1001@gmail.com>
2026-01-22 10:59:32 -0500

    rpg character lab

---
+++ Python Certification/build-an-rpg-character.py
@@ -0,0 +1,37 @@
+# ** start of main.py **
+
+full_dot = "●"
+empty_dot = "○"
+
+
+def create_character(name, strength, intelligence, charisma):
+    if not isinstance(name, str):
+        return "The character name should be a string"
+    if not name:
+        return "The character should have a name"
+    if len(name) > 10:
+        return "The character name is too long"
+    if " " in name:
+        return "The character name should not contain spaces"
+    if (
+        not isinstance(strength, int)
+        or not isinstance(intelligence, int)
+        or not isinstance(charisma, int)
+    ):
+        return "All stats should be integers"
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+    if strength + intelligence + charisma != 7:
+        return "The character should start with 7 points"
+
+    output = f"{name}\n"
+    output += f"STR {full_dot*strength}{empty_dot*(10-strength)}\n"
+    output += f"INT {full_dot*intelligence}{empty_dot*(10-intelligence)}\n"
+    output += f"CHA {full_dot*charisma}{empty_dot*(10-charisma)}"
+
+    return output
+
+
+# ** end of main.py **
